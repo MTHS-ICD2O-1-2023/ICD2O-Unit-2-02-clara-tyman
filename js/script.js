ba93e3dac70f59a05ff3c6cbc647efac290adfc2
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-18 20:05:50 +0000

    html js css

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+/**
+ * Do basic math.
+ */
+function doMathClicked() {
+  document.getElementById("add-math").innerHTML = "<p>32 + 21 = " + (32 + 21) + "</p>"
+  document.getElementById("subtract-math").innerHTML = "<p>27 - 13 =  " + (27 - 13) + "</p>"  
+  document.getElementById("multiply-math").innerHTML = "<p>8 + 5 × 3 = " + (8 + 5 * 3) + "</p>" 
+  document.getElementById("divide-math").innerHTML = "<p>(6 ÷ 4) + 5 = " + (6 / 4 + 5) + "</p>"
+  document.getElementById("exponent-math").innerHTML = "<p>7 + 3³ = " + (7 + 3 ** 3) + "</p>"
+}
